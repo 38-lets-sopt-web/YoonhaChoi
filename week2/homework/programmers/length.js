d63d6b9f853d00b37df34b445295c7e8415769c4
twossu <nadao202@naver.com>
2026-04-14 22:22:06 +0900

    feat: 프로그래머스 길이에 따른 연산

---
+++ week2/homework/programmers/length.js
@@ -0,0 +1,13 @@
+const long = [3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1];
+const short = [2, 3, 4, 5];
+
+function solution(num_list) {
+  if (num_list.length >= 11) {
+    return num_list.reduce((acc, cur) => acc + cur, 0);
+  } else {
+    return num_list.reduce((acc, cur) => acc * cur, 1);
+  }
+}
+
+console.log(solution(long));
+console.log(solution(short));
