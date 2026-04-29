30bde2ec11c08e4a73046ff26c85e3dc4735685f
twossu <nadao202@naver.com>
2026-04-30 04:09:53 +0900

    feat: level-dropdown ui 구현

--- week3/homework/src/components/game/game-board.jsx
+++ week3/homework/src/components/game/game-board.jsx
@@ -1,4 +1,21 @@
+import Button from "../common/button";
+import LevelDropdown from "../common/level-dropdown";
+
 const GameBoard = () => {
-  return <></>;
+  return (
+    <div className="bg-yellow-100 w-full rounded-xl p-5">
+      <div className="flex justify-between items-center mb-4">
+        <LevelDropdown />
+        <div className="flex gap-1.5">
+          <Button variant="start" onClick={() => {}}>
+            시작
+          </Button>
+          <Button variant="stop" onClick={() => {}}>
+            중지
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default GameBoard;
--- week3/homework/src/components/game/game-view.jsx
+++ week3/homework/src/components/game/game-view.jsx
@@ -3,10 +3,10 @@ import ScoreBoard from "./score-board";
 
 const GameView = () => {
   return (
-    <>
+    <div className="flex gap-5">
       <ScoreBoard />
       <GameBoard />
-    </>
+    </div>
   );
 };
 export default GameView;
