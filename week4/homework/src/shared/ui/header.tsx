cdeef99026145c77e667bae392c949444d10ca27
twossu <nadao202@naver.com>
2026-05-08 16:48:55 +0900

    feat: 헤더 이름, 로그아웃 기능

--- week4/homework/src/shared/ui/header.tsx
+++ week4/homework/src/shared/ui/header.tsx
@@ -2,11 +2,16 @@ import { Outlet, useNavigate } from "react-router";
 
 const Header = () => {
   const navigate = useNavigate();
+  const userName = localStorage.getItem("userId") ?? "";
+
+  const handleLogout = () => {
+    localStorage.removeItem("userId");
+    navigate("/login");
+  };
 
   const tabs = [
     { name: "내 정보", path: "/mypage" },
     { name: "회원 조회", path: "/members" },
-    { name: "로그아웃", path: "/logout" },
   ];
 
   return (
@@ -14,11 +19,10 @@ const Header = () => {
       <header className="flex justify-between px-20 py-6 bg-blue-950 text-white">
         <div className="flex flex-col gap-2">
           <h1 className="text-2xl font-bold">SOPT MEMBERS</h1>
-          <p>안녕하세요, 이수님!</p>
+          <p>안녕하세요. {userName}님!</p>
         </div>
-
         <nav className="flex gap-8">
-          <ul className="flex items-center gap-8  ">
+          <ul className="flex items-center gap-8">
             {tabs.map((tab) => (
               <li key={tab.name}>
                 <button
@@ -30,10 +34,18 @@ const Header = () => {
                 </button>
               </li>
             ))}
+            <li>
+              <button
+                type="button"
+                className="hover:text-gray-300 transition-colors"
+                onClick={handleLogout}
+              >
+                로그아웃
+              </button>
+            </li>
           </ul>
         </nav>
       </header>
-
       <main>
         <Outlet />
       </main>
