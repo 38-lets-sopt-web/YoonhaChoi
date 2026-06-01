16f8624764dbbc3578e85083e4eac47988d1d536
twossu <nadao202@naver.com>
2026-06-02 03:10:31 +0900

    feat: 영화 카드 컴포넌트 ui 구현

---
+++ Week6/src/pages/home/components/card.tsx
@@ -0,0 +1,21 @@
+interface CardProps {
+  title: string;
+  posterUrl: string;
+  date: string;
+  description: string;
+}
+
+function Card({ title, posterUrl, date, description }: CardProps) {
+  return (
+    <div className="cursor-pointer rounded-xl overflow-hidden bg-white min-w-[23rem]">
+      <img src={posterUrl} alt={title} className="w-full  object-cover" />
+      <div className="p-5">
+        <h3 className="label">{title}</h3>
+        <p className="caption-disabled mt-1">{date}</p>
+        <p className="caption mt-1 line-clamp-4">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
